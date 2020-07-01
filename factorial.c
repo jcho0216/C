a4434d17183974ef70e56acad4b6a380407fcb56
Cho Juneseo <chojuneseo1@gmail.com>
2020-07-01 20:19:03 +0900

    [docs 07.01 study ]

---
+++ factorial.c
@@ -0,0 +1,19 @@
+﻿
+
+#include <stdio.h>
+int factorial(int a) {
+
+	if (a == 1) return 1;
+	return a *= factorial(a - 1);
+
+}
+int main()
+{
+	int num;
+
+	scanf_s("%d", &num);
+
+	printf("%d", factorial(num));
+	
+	return 0;
+}
---
+++ fibonacci_ex.c
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int fibonacci(int n)
+{
+    if (n == 0)
+        return 0;
+    else if (n == 1)
+        return 1;
+    else return fibonacci(n - 1) + fibonacci(n - 2);
+
+
+}
+int main(void) {
+
+    int num;
+    scanf("%d", &num);
+    printf("%d", fibonacci(num));
+
+    return 0;
+    
+}
+
